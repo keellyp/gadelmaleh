b08d071e2b3d8454fe90a8d7e07a330312beb688
keellyp <kelly.phan@hetic.net>
2017-06-02 10:47:11 +0200

    :construction: Responsive mobile done
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+var burgerIcon = document.querySelector('.burger-icon');
+var burgerQuit = document.querySelector('.burger-quit');
+var burgerMenu = document.querySelector('.burger-menu');
+var isShow = false;
+burgerIcon.addEventListener('click', function()
+{
+    if (isShow == false)
+    {
+        burgerMenu.classList.add('animation-burger-menu');
+        isShow = true;
+    }
+}, false)
+
+burgerQuit.addEventListener('click', function()
+{
+    if (isShow)
+    {
+        burgerMenu.classList.remove('animation-burger-menu');
+        isShow = false;
+    }
+}, false)
